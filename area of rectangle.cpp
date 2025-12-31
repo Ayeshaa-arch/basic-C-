e2aa44e908d75a6630a3191596279dbb80288c84
Ayeshaa-arch <ayeshaahmad1955@gmail.com>
2025-12-31 12:27:41 +0500

    first commit

---
+++ area of rectangle.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int height, width,area;
+	height = 10;
+	width = 3;
+	area = height * width;
+	cout<<"Area of rectangle"<<" = "<<area;
+	return 0;
+}
