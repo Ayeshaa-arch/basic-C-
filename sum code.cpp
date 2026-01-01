c9337dca370b528cfdd8b99248267d4813f88d1c
Ayeshaa-arch <ayeshaahmad1955@gmail.com>
2026-01-01 23:17:22 +0500

    13th

---
+++ sum code.cpp
@@ -0,0 +1,16 @@
+//sum of 2 numbers
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b, sum;
+    a = 24;
+    b = 30;
+    sum = a + b;
+
+    cout << "Sum is: " << sum;
+
+    return 0;
+}
+
