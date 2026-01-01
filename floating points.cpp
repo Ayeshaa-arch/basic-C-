4d255f7360c2ba32f24f3d68dfb0b45d9db89715
Ayeshaa-arch <ayeshaahmad1955@gmail.com>
2026-01-01 22:49:16 +0500

    2nd

---
+++ floating points.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	float var1,var2,sum;
+	var1 = 23.74;
+	var2 = 3.52;
+	sum = var1 +  var2;
+	cout<<var1<<" + "<<var2<<" = "<<sum;
+	return 0;
+}
