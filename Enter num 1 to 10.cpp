78a430bb98a02ecbee29861eeb8cbf467e8bb667
Ayeshaa-arch <ayeshaahmad1955@gmail.com>
2026-01-21 20:00:19 +0500

    switch numbers display

---
+++ Enter num 1 to 10.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+	int num;
+	cout<<"Enter number 1 to 10 : ";
+	cin>>num;
+	switch (num)
+	{
+		case 1 :
+			cout<<"ONE";
+			break;
+		case 2 :
+			cout<<"TWO";
+			break;
+		case 3 :
+			cout<<"THREE";
+			break;
+		case 4 :
+			cout<<"FOUR";
+			break;
+		case 5 :
+			cout<<"FIVE";
+			break;
+		case 6 :
+			cout<<"SIX";
+			break;
+		case 7 :
+			cout<<"SEVEN";
+			break;
+		case 8 :
+			cout<<"EIGHT";
+			break;
+		case 9 :
+			cout<<"NINE";
+			break;
+		case 10 :
+			cout<<"TEN";
+			break;
+		default:
+			cout<<"Invalid number!";
+		
+	}
+	return 0;
+ } 
