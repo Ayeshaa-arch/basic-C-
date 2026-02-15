19db1f29d28d4e93dcae05ab1e645132361e3ef6
Ayeshaa-arch <ayeshaahmad1955@gmail.com>
2026-02-15 15:16:06 +0500

    loops

--- sum.cpp
+++ sum.cpp
@@ -1,15 +1,18 @@
-#include <iostream>
+//while loop
+#include<iostream>
 using namespace std;
 int main()
 {
-	int n ,a,b,sum;
-	cout<<"Enter 3 digit number = ";
-	cin>>n;
-	a = n/100;
-	n = n%100;
-	b = n/10;
-	n = n%10;
-	sum = a+b+n;
-	cout<<" Sum is : "<<sum;
+	int i,sum;
+	i=1;
+	sum=0;
+	do
+	{
+		cout<<i<<endl;
+		sum=sum+i;
+		i++;
+	}while(i<=5);
+	cout<<"Sum is "<<sum;
 	return 0;
-	}
+	
+}
